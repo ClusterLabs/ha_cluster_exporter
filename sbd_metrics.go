c6170d04f53e1e1ceba8cfca883931a73a35c6ee
dmaiocchi <dmaiocchi@suse.com>
2019-10-02 14:08:52 +0200

    Implement lookup of sbd device names
    Implement tests for this

--- sbd_metrics.go
+++ sbd_metrics.go
@@ -4,27 +4,35 @@ import (
 	"fmt"
 	"io/ioutil"
 	"os"
+	"regexp"
 	"strings"
 )
 
-// return a list of sbd devices
-func getSbdDevices() error {
-	// get ringStatus
+func readSdbFile() ([]byte, error) {
 
 	sbdConfFile, err := os.Open("/etc/sysconfig/sbd")
 	if err != nil {
-		return fmt.Errorf("[ERROR] Could not open sbd config file %s", err)
+		return nil, fmt.Errorf("[ERROR] Could not open sbd config file %s", err)
 	}
 
 	defer sbdConfFile.Close()
 	sbdConfigRaw, err := ioutil.ReadAll(sbdConfFile)
 
 	if err != nil {
-		return fmt.Errorf("[ERROR] Could not read sbd config file %s", err)
+		return nil, fmt.Errorf("[ERROR] Could not read sbd config file %s", err)
 	}
-	sbdDevicesRaw := strings.SplitAfter(string(sbdConfigRaw), "SBD_DEVICE=")
-	sbdDevices := strings.Trim(sbdDevicesRaw[1], "\"")
-	fmt.Printf("sbd devices:%s: ", sbdDevices)
+	return sbdConfigRaw, nil
+}
 
-	return nil
+// return a list of sbd devices that we get from config
+func getSbdDevices(sbdConfigRaw []byte) []string {
+	// in config it can be both SBD_DEVICE="/dev/foo" or SBD_DEVICE=/dev/foo;/dev/bro
+	wordOnly := regexp.MustCompile("SBD_DEVICE=\"?[a-zA-Z-/;]+\"?")
+	sbdDevicesConfig := wordOnly.FindString(string(sbdConfigRaw))
+	// remove the SBD_DEVICE
+	sbdArray := strings.Split(sbdDevicesConfig, "SBD_DEVICE=")[1]
+	// make a list of devices by ; seperators and remove double quotes if present
+	sbdDevices := strings.Split(strings.Trim(sbdArray, "\""), ";")
+	fmt.Println(sbdDevices)
+	return sbdDevices
 }
