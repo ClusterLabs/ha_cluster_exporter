73f99e47a0ac16894289cd619db581e172a9f02d
dmaiocchi <dmaiocchi@suse.com>
2019-10-11 13:02:39 +0200

    Add types for parsing json and tests

--- drbd_metrics.go
+++ drbd_metrics.go
@@ -5,6 +5,15 @@ import (
 	"os/exec"
 )
 
+// drbdStatus is for parsing relevant data we want to convert to metrics
+type drbdStatus struct {
+	Name    string `json:"name"`
+	Role    string `json:"role"`
+	Devices []struct {
+		Volume int `json:"volume"`
+	} `json:"devices"`
+}
+
 // return drbd status in byte raw json
 func getDrbdInfo() []byte {
 	// get ringStatus
--- drbd_metrics_test.go
+++ drbd_metrics_test.go
@@ -1,7 +1,9 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
+	"log"
 	"testing"
 )
 
@@ -10,4 +12,261 @@ import (
 func TestDrbdStatusFuncMinimalError(t *testing.T) {
 	fmt.Println("=== Testing DRBD : testing function to get infos")
 	getDrbdInfo()
+}
+
+func TestDrbdParsing(t *testing.T) {
+	var drbdDataRaw = []byte(` [
+		{
+		  "name": "1-single-0",
+		  "node-id": 2,
+		  "role": "Secondary",
+		  "suspended": false,
+		  "write-ordering": "flush",
+		  "devices": [
+			{
+			  "volume": 0,
+			  "minor": 2,
+			  "disk-state": "UpToDate",
+			  "client": false,
+			  "quorum": true,
+			  "size": 409600,
+			  "read": 0,
+			  "written": 548525,
+			  "al-writes": 4,
+			  "bm-writes": 0,
+			  "upper-pending": 0,
+			  "lower-pending": 0
+			} ],
+		  "connections": [
+			{
+			  "peer-node-id": 1,
+			  "name": "SLE15-sp1-gm-drbd1145296-node1",
+			  "connection-state": "Connected", 
+			  "congested": false,
+			  "peer-role": "Primary",
+			  "ap-in-flight": 0,
+			  "rs-in-flight": 0,
+			  "peer_devices": [
+				{
+				  "volume": 0,
+				  "replication-state": "Established",
+				  "peer-disk-state": "UpToDate",
+				  "peer-client": false,
+				  "resync-suspended": "no",
+				  "received": 548525,
+				  "sent": 0,
+				  "out-of-sync": 0,
+				  "pending": 0,
+				  "unacked": 0,
+				  "has-sync-details": false,
+				  "has-online-verify-details": false,
+				  "percent-in-sync": 100.00
+				} ]
+			} ]
+		}
+		,
+		{
+		  "name": "1-single-1",
+		  "node-id": 2,
+		  "role": "Secondary",
+		  "suspended": false,
+		  "write-ordering": "flush",
+		  "devices": [
+			{
+			  "volume": 0,
+			  "minor": 3,
+			  "disk-state": "UpToDate",
+			  "client": false,
+			  "quorum": true,
+			  "size": 10200,
+			  "read": 0,
+			  "written": 546005,
+			  "al-writes": 1,
+			  "bm-writes": 0,
+			  "upper-pending": 0,
+			  "lower-pending": 0
+			} ],
+		  "connections": [
+			{
+			  "peer-node-id": 1,
+			  "name": "SLE15-sp1-gm-drbd1145296-node1",
+			  "connection-state": "Connected", 
+			  "congested": false,
+			  "peer-role": "Primary",
+			  "ap-in-flight": 0,
+			  "rs-in-flight": 0,
+			  "peer_devices": [
+				{
+				  "volume": 0,
+				  "replication-state": "Established",
+				  "peer-disk-state": "UpToDate",
+				  "peer-client": false,
+				  "resync-suspended": "no",
+				  "received": 546005,
+				  "sent": 0,
+				  "out-of-sync": 0,
+				  "pending": 0,
+				  "unacked": 0,
+				  "has-sync-details": false,
+				  "has-online-verify-details": false,
+				  "percent-in-sync": 100.00
+				} ]
+			} ]
+		}
+		,
+		{
+		  "name": "vg1",
+		  "node-id": 2,
+		  "role": "Secondary",
+		  "suspended": false,
+		  "write-ordering": "flush",
+		  "devices": [
+			{
+			  "volume": 0,
+			  "minor": 6,
+			  "disk-state": "UpToDate",
+			  "client": false,
+			  "quorum": true,
+			  "size": 409548,
+			  "read": 0,
+			  "written": 548525,
+			  "al-writes": 4,
+			  "bm-writes": 0,
+			  "upper-pending": 0,
+			  "lower-pending": 0
+			} ],
+		  "connections": [
+			{
+			  "peer-node-id": 1,
+			  "name": "SLE15-sp1-gm-drbd1145296-node1",
+			  "connection-state": "Connected", 
+			  "congested": false,
+			  "peer-role": "Primary",
+			  "ap-in-flight": 0,
+			  "rs-in-flight": 0,
+			  "peer_devices": [
+				{
+				  "volume": 0,
+				  "replication-state": "Established",
+				  "peer-disk-state": "UpToDate",
+				  "peer-client": false,
+				  "resync-suspended": "no",
+				  "received": 548525,
+				  "sent": 0,
+				  "out-of-sync": 0,
+				  "pending": 0,
+				  "unacked": 0,
+				  "has-sync-details": false,
+				  "has-online-verify-details": false,
+				  "percent-in-sync": 100.00
+				} ]
+			} ]
+		}
+		,
+		{
+		  "name": "vg2",
+		  "node-id": 2,
+		  "role": "Secondary",
+		  "suspended": false,
+		  "write-ordering": "flush",
+		  "devices": [
+			{
+			  "volume": 0,
+			  "minor": 7,
+			  "disk-state": "UpToDate",
+			  "client": false,
+			  "quorum": true,
+			  "size": 307152,
+			  "read": 0,
+			  "written": 548541,
+			  "al-writes": 4,
+			  "bm-writes": 0,
+			  "upper-pending": 0,
+			  "lower-pending": 0
+			} ],
+		  "connections": [
+			{
+			  "peer-node-id": 1,
+			  "name": "SLE15-sp1-gm-drbd1145296-node1",
+			  "connection-state": "Connected", 
+			  "congested": false,
+			  "peer-role": "Primary",
+			  "ap-in-flight": 0,
+			  "rs-in-flight": 0,
+			  "peer_devices": [
+				{
+				  "volume": 0,
+				  "replication-state": "Established",
+				  "peer-disk-state": "UpToDate",
+				  "peer-client": false,
+				  "resync-suspended": "no",
+				  "received": 548541,
+				  "sent": 0,
+				  "out-of-sync": 0,
+				  "pending": 0,
+				  "unacked": 0,
+				  "has-sync-details": false,
+				  "has-online-verify-details": false,
+				  "percent-in-sync": 100.00
+				} ]
+			} ]
+		}
+		,
+		{
+		  "name": "vg3",
+		  "node-id": 2,
+		  "role": "Secondary",
+		  "suspended": false,
+		  "write-ordering": "flush",
+		  "devices": [
+			{
+			  "volume": 0,
+			  "minor": 8,
+			  "disk-state": "UpToDate",
+			  "client": false,
+			  "quorum": true,
+			  "size": 204756,
+			  "read": 0,
+			  "written": 548576,
+			  "al-writes": 3,
+			  "bm-writes": 0,
+			  "upper-pending": 0,
+			  "lower-pending": 0
+			} ],
+		  "connections": [
+			{
+			  "peer-node-id": 1,
+			  "name": "SLE15-sp1-gm-drbd1145296-node1",
+			  "connection-state": "Connected", 
+			  "congested": false,
+			  "peer-role": "Primary",
+			  "ap-in-flight": 0,
+			  "rs-in-flight": 0,
+			  "peer_devices": [
+				{
+				  "volume": 0,
+				  "replication-state": "Established",
+				  "peer-disk-state": "UpToDate",
+				  "peer-client": false,
+				  "resync-suspended": "no",
+				  "received": 548576,
+				  "sent": 0,
+				  "out-of-sync": 0,
+				  "pending": 0,
+				  "unacked": 0,
+				  "has-sync-details": false,
+				  "has-online-verify-details": false,
+				  "percent-in-sync": 100.00
+				} ]
+			} ]
+		}
+		]`)
+
+	var drbdDevs []drbdStatus
+	err := json.Unmarshal(drbdDataRaw, &drbdDevs)
+	if err != nil {
+		log.Fatalln("[ERROR]:", err)
+	}
+	log.Println(drbdDevs[0].Devices[0].Volume)
+
 }
