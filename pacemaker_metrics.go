08d58daa05c814ced80c721cbf1d63ead3c5df23
Stefano Torresi <stefano.torresi@suse.com>
2019-10-17 14:23:28 +0200

    implement custom collector draft

--- pacemaker_metrics.go
+++ pacemaker_metrics.go
@@ -2,8 +2,12 @@ package main
 
 import (
 	"encoding/xml"
+	"os/exec"
+	"time"
 
 	"github.com/pkg/errors"
+	"github.com/prometheus/client_golang/prometheus"
+	log "github.com/sirupsen/logrus"
 )
 
 // this types are for reading pacemaker configuration xml when running crm_mon command
@@ -68,4 +72,57 @@ func parsePacemakerStatus(pacemakerXMLRaw []byte) (pacemakerStatus, error) {
 		return pacemakerStat, errors.Wrap(err, "could not parse Pacemaker status from XML")
 	}
 	return pacemakerStat, nil
+}
+
+type metricsGroup map[string]*prometheus.Desc
+
+type pacemakerCollector struct {
+	metrics metricsGroup
+}
+
+func NewPacemakerCollector() *pacemakerCollector {
+	return &pacemakerCollector{
+		metrics: metricsGroup{
+			"nodes_total": prometheus.NewDesc(prometheus.BuildFQName(NAMESPACE, "nodes", "total"), "Total number of nodes in the cluster", nil, nil),
+		},
+	}
+}
+
+func (c *pacemakerCollector) makeMetric(metricKey string, valueType prometheus.ValueType, value float64) (prometheus.Metric, error) {
+	metric, err := prometheus.NewConstMetric(c.metrics[metricKey], valueType, value)
+	if err != nil {
+		return nil, errors.Wrap(err, "could not build a new metric")
+	}
+	return prometheus.NewMetricWithTimestamp(time.Now(), metric), nil
+}
+
+func (c *pacemakerCollector) Describe(ch chan<- *prometheus.Desc) {
+	for _, metric := range c.metrics {
+		ch <- metric
+	}
+}
+
+func (c *pacemakerCollector) Collect(ch chan<- prometheus.Metric) {
+	// get cluster status xml
+	log.Infoln("Reading cluster status with crm_mon...")
+	pacemakerXMLRaw, err := exec.Command("/usr/sbin/crm_mon", "-1", "--as-xml", "--group-by-node", "--inactive").Output()
+	if err != nil {
+		log.Warnln(err)
+		return
+	}
+
+	// parse raw XML returned from crm_mon and populate structs for metrics
+	data, err := parsePacemakerStatus(pacemakerXMLRaw)
+	if err != nil {
+		log.Warnln(err)
+		return
+	}
+
+	nodesTotal, err := prometheus.NewConstMetric(c.metrics["nodes_total"], prometheus.GaugeValue, float64(data.Summary.Nodes.Number))
+	nodesTotal = prometheus.NewMetricWithTimestamp(time.Now(), nodesTotal)
+	if err != nil {
+		log.Warnln(err)
+		return
+	}
+	ch <- nodesTotal
 }
