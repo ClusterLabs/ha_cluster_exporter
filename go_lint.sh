bd8a67e6ce638dd73362661b34a3c7402e037f86
Sathia <sathia.s@getsimpl.com>
2019-10-17 22:50:08 +0530

    chore(#57): add new stage for go fmt check

---
+++ go_lint.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# NOTE: This script needs be run from the root of the GD2 repository
+
+# Find all Go source files in the repository, that are not vendored or generated
+# and then run gofmt on them
+GOFMT_FILE_LIST=$(gofmt -l . | grep -v vendor)
+if [ -n "${GOFMT_FILE_LIST}" ]; then
+  printf >&2 'gofmt failed for the following files:\n%s\n\nplease run "gofmt -w ." on your changes before committing.\n' "${GOFMT_FILE_LIST}"
+  exit 1
+fi
