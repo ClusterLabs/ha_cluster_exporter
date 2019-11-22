66b2a05e27e13a444086969d32df7f40b24d8193
dmaiocchi <dmaiocchi@suse.com>
2019-11-22 14:04:51 +0100

    Implement splitbrain metric

--- drbd_metrics.go
+++ drbd_metrics.go
@@ -2,7 +2,9 @@ package main
 
 import (
 	"encoding/json"
+	"io/ioutil"
 	"os/exec"
+	"path/filepath"
 	"strconv"
 	"strings"
 
@@ -37,7 +39,7 @@ var (
 		"resources":        NewMetricDesc("drbd", "resources", "The DRBD resources; 1 line per name, per volume", []string{"resource", "role", "volume", "disk_state"}),
 		"connections":      NewMetricDesc("drbd", "connections", "The DRBD resource connections; 1 line per per resource, per peer_node_id", []string{"resource", "peer_node_id", "peer_role", "volume", "peer_disk_state"}),
 		"connections_sync": NewMetricDesc("drbd", "connections_sync", "The in sync percentage value for DRBD resource connections", []string{"resource", "peer_node_id", "volume"}),
-		"split_brain":      NewMetricDesc("drbd", "split_brain", "The split brain metric per node, resource and volume. The value 1 indicate the split brain is present", []string{"node", "resource", "volume"}),
+		"split_brain":      NewMetricDesc("drbd", "split_brain", "The split brain metric per node, resource and volume. The value 1 indicate the split brain is present", []string{"resource", "volume"}),
 	}
 )
 
@@ -65,6 +67,24 @@ func (c *drbdCollector) Collect(ch chan<- prometheus.Metric) {
 	defer c.mutex.Unlock()
 
 	log.Infoln("Collecting DRBD metrics...")
+
+	// set split brain metric
+	// by default if the custom hook is not set, the exporter will not be able to detect it.
+	files, err := ioutil.ReadDir("/var/run/drbd/splitbrain")
+	if err != nil {
+		log.Warnf("Error while reading directory /var/run/drbd/splitbrain: %s", err)
+	} else {
+		for _, f := range files {
+			// check if in directory there are file of syntax we expect (nil is when there is not any)
+			match, _ := filepath.Glob("/var/run/drbd/splitbrain/drbd-split-brain-detected*")
+			if match != nil {
+				resAndVolume := strings.Split(f.Name(), "drbd-split-brain-detected-")[1]
+				resource := strings.Split(resAndVolume, "-")[0]
+				volume := strings.Split(resAndVolume, "-")[1]
+				ch <- c.makeGaugeMetric("split_brain", float64(1), resource, volume)
+			}
+		}
+	}
 
 	drbdStatusRaw, err := exec.Command(c.drbdsetupPath, "status", "--json").Output()
 	if err != nil {
@@ -103,15 +123,6 @@ func (c *drbdCollector) Collect(ch chan<- prometheus.Metric) {
 		}
 	}
 
-	// set split brain metric
-
-	// 1) loop over location /var/lib/drbd/drbd-split-brain-detected-$DRBD_RESOURCE-$DRBD_VOLUME
-	// 2) after prefix loop over-filenames and create metrics based on file name
-	// 3) go throught the founded split-brain and set the metric accordingly (if there is no splitbrain just don't set to 0, as the overall design of exporter)
-
-	// for foundedSplitBrains do
-	// ch <- c.makeGaugeMetric("split_brain", float64(1), splitBrain["node"], splitBrain["resource"], splitBrain["volume"])
-
 }
 
 func parseDrbdStatus(statusRaw []byte) ([]drbdStatus, error) {
@@ -121,11 +132,4 @@ func parseDrbdStatus(statusRaw []byte) ([]drbdStatus, error) {
 		return drbdDevs, err
 	}
 	return drbdDevs, nil
-}
-
-// this function basically depends on the custom hook for drbd.
-// by default if the custom hook is not set, the exporter will not be able to detect it.
-// return a map of splitbrains
-func parseDrbdSplitBrains() {
-	log.Info("not yet")
 }
