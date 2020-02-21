a783ac2cf9dabba0fcf8fb9fe02e196590150ea0
Stefano Torresi <stefano.torresi@suse.com>
2020-02-21 15:15:19 +0100

    Refactor Pacemaker collector (#132)
    * add the nodes section of the CIB
    
    * add resources to CIB structs
    
    * refactor cib and crmmon parsing in separate packages
    
    * fix struct indentations
    
    * update design docs
    
    * use 'subsystem' word more consistently

---
+++ collector/pacemaker/cib/parser_test.go
@@ -0,0 +1,63 @@
+package cib
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestConstructor(t *testing.T) {
+	p := NewCibAdminParser("foo")
+	assert.Equal(t, "foo", p.cibAdminPath)
+}
+
+func TestParse(t *testing.T) {
+	p := NewCibAdminParser("../../../test/fake_cibadmin.sh")
+	data, err := p.Parse()
+	assert.NoError(t, err)
+	assert.Equal(t, 2, len(data.Configuration.Nodes))
+	assert.Equal(t, "hana01", data.Configuration.Nodes[0].Uname)
+	assert.Equal(t, "hana02", data.Configuration.Nodes[1].Uname)
+	assert.Equal(t, 3, len(data.Configuration.Resources.Primitives))
+	assert.Equal(t, 1, len(data.Configuration.Resources.Masters))
+	assert.Equal(t, 1, len(data.Configuration.Resources.Clones))
+	assert.Equal(t, "stonith-sbd", data.Configuration.Resources.Primitives[0].Id)
+	assert.Equal(t, "stonith", data.Configuration.Resources.Primitives[0].Class)
+	assert.Equal(t, "external/sbd", data.Configuration.Resources.Primitives[0].Type)
+	assert.Equal(t, "stonith-sbd-instance_attributes", data.Configuration.Resources.Primitives[0].InstanceAttributes.Id)
+	assert.Equal(t, 1, len(data.Configuration.Resources.Primitives[0].InstanceAttributes.NvPairs))
+	assert.Equal(t, "pcmk_delay_max", data.Configuration.Resources.Primitives[0].InstanceAttributes.NvPairs[0].Name)
+	assert.Equal(t, "stonith-sbd-instance_attributes-pcmk_delay_max", data.Configuration.Resources.Primitives[0].InstanceAttributes.NvPairs[0].Id)
+	assert.Equal(t, "30s", data.Configuration.Resources.Primitives[0].InstanceAttributes.NvPairs[0].Value)
+	assert.Equal(t, "msl_SAPHana_PRD_HDB00", data.Configuration.Resources.Masters[0].Id)
+	assert.Equal(t, 3, len(data.Configuration.Resources.Masters[0].MetaAttributes.NvPairs))
+	assert.Equal(t, "rsc_SAPHana_PRD_HDB00", data.Configuration.Resources.Masters[0].Primitive.Id)
+	assert.Equal(t, 5, len(data.Configuration.Resources.Masters[0].Primitive.Operations))
+	assert.Equal(t, "rsc_SAPHana_PRD_HDB00-start-0", data.Configuration.Resources.Masters[0].Primitive.Operations[0].Id)
+	assert.Equal(t, "start", data.Configuration.Resources.Masters[0].Primitive.Operations[0].Name)
+	assert.Equal(t, 0, data.Configuration.Resources.Masters[0].Primitive.Operations[0].Interval)
+	assert.Equal(t, 3600, data.Configuration.Resources.Masters[0].Primitive.Operations[0].Timeout)
+	assert.Equal(t, "rsc_SAPHana_PRD_HDB00-stop-0", data.Configuration.Resources.Masters[0].Primitive.Operations[1].Id)
+	assert.Equal(t, "stop", data.Configuration.Resources.Masters[0].Primitive.Operations[1].Name)
+	assert.Equal(t, 0, data.Configuration.Resources.Masters[0].Primitive.Operations[1].Interval)
+	assert.Equal(t, 3600, data.Configuration.Resources.Masters[0].Primitive.Operations[1].Timeout)
+	assert.Equal(t, "rsc_SAPHana_PRD_HDB00-promote-0", data.Configuration.Resources.Masters[0].Primitive.Operations[2].Id)
+	assert.Equal(t, "promote", data.Configuration.Resources.Masters[0].Primitive.Operations[2].Name)
+	assert.Equal(t, 0, data.Configuration.Resources.Masters[0].Primitive.Operations[2].Interval)
+	assert.Equal(t, 3600, data.Configuration.Resources.Masters[0].Primitive.Operations[2].Timeout)
+	assert.Equal(t, "rsc_SAPHana_PRD_HDB00-monitor-60", data.Configuration.Resources.Masters[0].Primitive.Operations[3].Id)
+	assert.Equal(t, "monitor", data.Configuration.Resources.Masters[0].Primitive.Operations[3].Name)
+	assert.Equal(t, "Master", data.Configuration.Resources.Masters[0].Primitive.Operations[3].Role)
+	assert.Equal(t, 60, data.Configuration.Resources.Masters[0].Primitive.Operations[3].Interval)
+	assert.Equal(t, 700, data.Configuration.Resources.Masters[0].Primitive.Operations[3].Timeout)
+	assert.Equal(t, "rsc_SAPHana_PRD_HDB00-monitor-61", data.Configuration.Resources.Masters[0].Primitive.Operations[4].Id)
+	assert.Equal(t, "monitor", data.Configuration.Resources.Masters[0].Primitive.Operations[4].Name)
+	assert.Equal(t, "Slave", data.Configuration.Resources.Masters[0].Primitive.Operations[4].Role)
+	assert.Equal(t, 61, data.Configuration.Resources.Masters[0].Primitive.Operations[4].Interval)
+	assert.Equal(t, 700, data.Configuration.Resources.Masters[0].Primitive.Operations[4].Timeout)
+	assert.Equal(t, "test", data.Configuration.Resources.Primitives[2].Id)
+	assert.Equal(t, "ocf", data.Configuration.Resources.Primitives[2].Class)
+	assert.Equal(t, "heartbeat", data.Configuration.Resources.Primitives[2].Provider)
+	assert.Equal(t, "Dummy", data.Configuration.Resources.Primitives[2].Type)
+
+}
---
+++ collector/pacemaker/crmmon/parser_test.go
@@ -0,0 +1,50 @@
+package crmmon
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestConstructor(t *testing.T) {
+	p := NewCrmMonParser("foo")
+	assert.Equal(t, "foo", p.crmMonPath)
+}
+
+func TestParse(t *testing.T) {
+	p := NewCrmMonParser("../../../test/fake_crm_mon.sh")
+	data, err := p.Parse()
+	assert.NoError(t, err)
+	assert.Equal(t, "2.0.0", data.Version)
+	assert.Equal(t, 6, data.Summary.Resources.Number)
+	assert.Equal(t, 0, data.Summary.Resources.Disabled)
+	assert.Equal(t, 0, data.Summary.Resources.Blocked)
+	assert.Equal(t, "Fri Oct 18 11:48:22 2019", data.Summary.LastChange.Time)
+	assert.Equal(t, 2, data.Summary.Nodes.Number)
+	assert.Equal(t, "hana01", data.Nodes[0].Name)
+	assert.Equal(t, "1084783375", data.Nodes[0].ID)
+	assert.Equal(t, true, data.Nodes[0].Online)
+	assert.Equal(t, true, data.Nodes[0].ExpectedUp)
+	assert.Equal(t, true, data.Nodes[0].DC)
+	assert.Equal(t, false, data.Nodes[0].Unclean)
+	assert.Equal(t, false, data.Nodes[0].Shutdown)
+	assert.Equal(t, false, data.Nodes[0].StandbyOnFail)
+	assert.Equal(t, false, data.Nodes[0].Maintenance)
+	assert.Equal(t, false, data.Nodes[0].Pending)
+	assert.Equal(t, false, data.Nodes[0].Standby)
+	assert.Equal(t, "hana02", data.Nodes[1].Name)
+	assert.Equal(t, "1084783376", data.Nodes[1].ID)
+	assert.Equal(t, true, data.Nodes[1].Online)
+	assert.Equal(t, true, data.Nodes[1].ExpectedUp)
+	assert.Equal(t, false, data.Nodes[1].DC)
+	assert.Equal(t, false, data.Nodes[1].Unclean)
+	assert.Equal(t, false, data.Nodes[1].Shutdown)
+	assert.Equal(t, false, data.Nodes[1].StandbyOnFail)
+	assert.Equal(t, false, data.Nodes[1].Maintenance)
+	assert.Equal(t, false, data.Nodes[1].Pending)
+	assert.Equal(t, false, data.Nodes[1].Standby)
+	assert.Equal(t, "hana01", data.NodeHistory.Node[0].Name)
+	assert.Equal(t, 5000, data.NodeHistory.Node[0].ResourceHistory[0].MigrationThreshold)
+	assert.Equal(t, 2, data.NodeHistory.Node[0].ResourceHistory[1].FailCount)
+	assert.Equal(t, "rsc_SAPHana_PRD_HDB00", data.NodeHistory.Node[0].ResourceHistory[0].Name)
+}
