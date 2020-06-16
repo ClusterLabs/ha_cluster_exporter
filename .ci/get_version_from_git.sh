ed0f4995a44f34b9928b44f6c34c0865d1b87517
Stefano Torresi <stefano.torresi@suse.com>
2020-06-16 14:42:40 +0200

    add number of commits since tags to development versions

--- .ci/get_version_from_git.sh
+++ .ci/get_version_from_git.sh
@@ -1,10 +1,13 @@
 #!/bin/sh
 TAG=$(git describe --tags --abbrev=0 2>/dev/null)
 
-if [ -z "${TAG}" ]; then
-  TAG="0.0dev"
-elif [ "$(git rev-list ${TAG}.. --count)" -gt 0 ]; then
-  TAG="${TAG}dev"
+if [ -n "${TAG}" ]; then
+  COMMITS_SINCE_TAG=$(git rev-list ${TAG}.. --count)
+  if [ "${COMMITS_SINCE_TAG}" -gt 0 ]; then
+    TAG="${TAG}.dev${COMMITS_SINCE_TAG}"
+  fi
+else
+  TAG="0"
 fi
 
 SUFFIX=$(git show -s --format=%ct.%h HEAD)
